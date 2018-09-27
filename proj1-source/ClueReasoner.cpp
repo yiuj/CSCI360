ed31a4f6ffef95ba1b844fcbdf52f69959bc761f
Jonathan Yiu <jonathanyiu@MyMacs-MacBook-Pro.local>
2018-09-27 15:53:35 -0700

    working

--- proj1-source/ClueReasoner.cpp
+++ proj1-source/ClueReasoner.cpp
@@ -116,35 +116,75 @@ void ClueReasoner::AddInitialClauses()
 	int roo_start = wea_end;
 	int roo_end = roo_start + num_rooms;
 
+	Clause clause;
 	for(int sus_c = sus_start; sus_c < sus_end; sus_c++) { // Iterate over suspect cards
-		Clause clause;
 		clause.push_back(GetPairNum(cf,sus_c));
-		solver->AddClause(clause);
 	}
+	solver->AddClause(clause);
+
+	clause.clear();
 	for(int wea_c = wea_start; wea_c < wea_end; wea_c++) { // Iterate over weapon cards
-		Clause clause;
 		clause.push_back(GetPairNum(cf,wea_c));
-		solver->AddClause(clause);
 	}
+	solver->AddClause(clause);
+
+	clause.clear();
 	for(int roo_c = roo_start; roo_c < roo_end; roo_c++) { // Iterate over room cards
-		Clause clause;
 		clause.push_back(GetPairNum(cf,roo_c));
-		solver->AddClause(clause);
 	}
+	solver->AddClause(clause);
 
 	// No two cards in each category can both be in the case file.
 	// TO BE IMPLEMENTED AS AN EXERCISE
+	for(int sus_c = sus_start; sus_c < sus_end; sus_c++) { // Iterate over suspect cards
+		for(int sus_cx = sus_start; sus_cx < sus_end; sus_cx++) { // Iterate over suspect cards
+			if(sus_c!=sus_cx) {
+				Clause clause;
+				clause.push_back(GetPairNum(cf,sus_c) * (-1));
+				clause.push_back(GetPairNum(cf,sus_cx) * (-1));
+				solver->AddClause(clause);
+				// cout << cards[sus_c] << GetPairNum(cf,sus_c) * (-1) << " ??? " << cards[sus_cx] << endl;
+			}
+		}
+	}
+	for(int wea_c = wea_start; wea_c < wea_end; wea_c++) { // Iterate over suspect cards
+		for(int wea_cx = wea_start; wea_cx < wea_end; wea_cx++) { // Iterate over suspect cards
+			if(wea_c!=wea_cx) {
+				Clause clause;
+				clause.push_back(GetPairNum(cf,wea_c) * (-1));
+				clause.push_back(GetPairNum(cf,wea_cx) * (-1));
+				solver->AddClause(clause);
+				// cout << cards[wea_c] << GetPairNum(cf,wea_c) * (-1) << " ??? " << cards[wea_cx] << endl;
+			}
+		}
+	}
+	for(int roo_c = roo_start; roo_c < roo_end; roo_c++) { // Iterate over suspect cards
+		for(int roo_cx = roo_start; roo_cx < roo_end; roo_cx++) { // Iterate over suspect cards
+			if(roo_c!=roo_cx) {
+				Clause clause;
+				clause.push_back(GetPairNum(cf,roo_c) * (-1));
+				clause.push_back(GetPairNum(cf,roo_cx) * (-1));
+				solver->AddClause(clause);
+				// cout << cards[roo_c] << GetPairNum(cf,roo_c) * (-1) << " ??? " << cards[roo_cx] << endl;
+			}
+		}
+	}
 }
 
-void ClueReasoner::Hand(string player, string cards[3])
+void ClueReasoner::Hand(string player, string hand_cards[3])
 {
 	// GetPlayerNum returns the index of the player in the players array (not the suspects array). 
 	// Remember that the players array is sorted wrt the order that the players play. 
 	// Also note that, player_num (not to be confused with num_players) is a private variable of the 
 	// ClueReasoner class that is initialized when this function is called.
-	player_num = GetPlayerNum(player);
+	// player_num = GetPlayerNum(player);
 	
 	// TO BE IMPLEMENTED AS AN EXERCISE
+	for(int i=0; i<3; i++) {
+		Clause clause;
+		clause.push_back(GetPairNum(player, hand_cards[i]));
+		solver->AddClause(clause);
+	}
 }
 
 void ClueReasoner::Suggest(string suggester, string card1, string card2, string card3, string refuter, string card_shown)
@@ -154,6 +194,94 @@ void ClueReasoner::Suggest(string suggester, string card1, string card2, string 
 	// To check if refuter is NULL or card_shown is NULL, you should use if(refuter == "") or if(card_shown == ""), respectively.
 	
 	// TO BE IMPLEMENTED AS AN EXERCISE
+	// your turn and someone refutes
+	int suggester_num = GetPlayerNum(suggester);
+	int refuter_num = GetPlayerNum(refuter);
+
+	if(refuter != "" && card_shown != "") {
+		int tracker = suggester_num;
+		tracker++;
+		if(tracker >= num_players) {
+			tracker = 0;
+		}
+		while(refuter != players[tracker]) {
+			//players[tracker] has none of the 3 cards
+			Clause clause;
+			clause.push_back(GetPairNum(players[tracker], card1) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			clause.push_back(GetPairNum(players[tracker], card2) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			clause.push_back(GetPairNum(players[tracker], card3) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			tracker++;
+			if(tracker >= num_players) {
+				tracker = 0;
+			}
+		}
+
+		Clause clause;
+		clause.push_back(GetPairNum(refuter, card_shown));
+		solver->AddClause(clause);
+	}
+
+	// not your turn and someone refutes
+	else if(refuter != "" && card_shown == "") {
+		int tracker = suggester_num;
+		tracker++;
+		if(tracker >= num_players) {
+			tracker = 0;
+		}
+		while(refuter != players[tracker]) {
+			//players[tracker] has none of the 3 cards
+			Clause clause;
+			clause.push_back(GetPairNum(players[tracker], card1) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			clause.push_back(GetPairNum(players[tracker], card2) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			clause.push_back(GetPairNum(players[tracker], card3) * (-1));
+			solver->AddClause(clause);
+			clause.clear();
+
+			tracker++;
+			if(tracker >= num_players) {
+				tracker = 0;
+			}
+		}
+
+		Clause clause;
+		clause.push_back(GetPairNum(refuter, card1));
+		clause.push_back(GetPairNum(refuter, card2));
+		clause.push_back(GetPairNum(refuter, card3));
+		solver->AddClause(clause);
+	}
+
+	// no one refutes --> your cards or cf
+	else if(refuter == "") {
+		Clause clause;
+		clause.push_back(GetPairNum(suggester, card1));
+		clause.push_back(GetPairNum(case_file, card1));
+		solver->AddClause(clause);
+
+		clause.clear();
+		clause.push_back(GetPairNum(suggester, card1));
+		clause.push_back(GetPairNum(case_file, card1));
+		solver->AddClause(clause);
+		
+		clause.clear();
+		clause.push_back(GetPairNum(suggester, card1));
+		clause.push_back(GetPairNum(case_file, card1));
+		solver->AddClause(clause);
+	}
 }
 
 void ClueReasoner::Accuse(string suggester, string card1, string card2, string card3, bool is_correct)
